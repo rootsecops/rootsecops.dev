45393a6dcf73fe586fbb63bfbf5c990202acb310
rootsecops <194398882+rootsecops@users.noreply.github.com>
2025-08-18 10:17:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.shields.io',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
